47352ce122c9adc4d2bfc35641666d255c82d99a
Bryan Flores <bryanflores12345@gmail.com>
2022-06-27 12:18:11 -0400

    Intro component

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Batman from '../../assets/batman.png';
 
 const Intro = () => {
   return (
@@ -14,9 +15,21 @@ const Intro = () => {
             <div className="slider-item">Movie Buff</div>
             <div className="slider-item">Gamer</div>
           </div>
+          <p className="desc">
+            I'm a Computer Science student from Montreal who graduated from the
+            Western Governors University in June 2022. I will be pursuing a
+            Master's degree in the same field at Georgia Tech (OMSCS) starting
+            Fall 2022. I love meeting new people and my favourite hobbies
+            include going out, watching movies and playing video games. I am
+            currently open to entry level software development opportunities or
+            internships.
+          </p>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="right-bg"></div>
+        <img src={Batman} alt="Lego Batman" className="batman" />
+      </div>
     </div>
   );
 };
