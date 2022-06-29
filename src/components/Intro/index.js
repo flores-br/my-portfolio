6e4f6d4e250e08ea9d99f86b5a335a218b84bc95
Bryan Flores <bryanflores12345@gmail.com>
2022-06-29 13:55:31 -0400

    open cv in new tab

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -11,7 +11,11 @@ const Intro = () => {
           <h2 className="intro-text">Hello, world! My name is</h2>
           <h1 className="intro-name">
             Bryan Flores
-            <a href="https://drive.google.com/file/d/1ykJ6ZVQHWoX2jwE-xPXaE2bpcfyb0dZl/view?usp=sharing">
+            <a
+              href="https://drive.google.com/file/d/1ykJ6ZVQHWoX2jwE-xPXaE2bpcfyb0dZl/view?usp=sharing"
+              target="blank"
+              rel="noreferrer"
+            >
               <AiOutlineFilePdf className="pdf" />
             </a>
           </h1>
